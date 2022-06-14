389eb2898f2219755092bbf04ef611c42e31bfaa
Michael Leahey <michael.leahey1843@outlook.com>
2022-06-14 12:23:06 -0400

    update

--- src/main/java/org/example/Product.java
+++ src/main/java/org/example/Product.java
@@ -21,4 +21,52 @@ public class Product {
         this.color = color;
     }
 
+
+    //getters and setters
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    //price
+    public Double getPrice() {
+        return price;
+    }
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    //categories
+    public String getCategory() {
+        return category;
+    }
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    //Quantity
+    public Integer getQuantity() {
+        return quantity;
+    }
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    //author
+    public String getAuthor() {
+        return author;
+    }
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    //color
+    public String getColor() {
+        return color;
+    }
+    public void setColor(String color) {
+        this.color = color;
+    }
 }
